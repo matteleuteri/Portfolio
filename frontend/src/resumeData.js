9bb76fd3b16922c66c9f3f76ac61b3ce50e26788
Matthew Eleuteri <meleuteri2@gmail.com>
2022-07-31 21:02:57 -0400

    initial commit

---
+++ frontend/src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Matthew Eleuteri",
+  "role": "Software Engineer",
+  // "linkedinId":"Your LinkedIn Id",
+  // "skypeid": "Your skypeid",
+  "roleDescription": "I am java developer at an insurance company",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I recently finished my M.S. in Computer Science and have been professionally developing software for two years.",
+  "address":"Boston Massachusetts",
+  "website":"https://www.google.com",
+  "education":[
+    {
+      "UniversityName":"University of Massachusetts ,Amherst",
+      "Major":"Computer Science and Math",
+      "MonthOfPassing":"Jun",
+      "YearOfPassing":"2020",
+      // "Achievements":"Some Achievements"
+    }// ,
+    // {
+    //   "UniversityName":"Some University",
+    //   "specialization":"Some specialization",
+    //   "MonthOfPassing":"Jan",
+    //   "YearOfPassing":"2018",
+    //   "Achievements":"Some Achievements"
+    // }
+  ],
+  "work":[
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+export default resumeData
